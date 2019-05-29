8cbdd31f6fc7929d8b55c5047f471f7c3de0d62a
Dan Morse <morsedan@outlook.com>
2019-05-29 10:39:48 -0700

    Initial Commit

---
+++ Amiibo/Model Controller/AmiiboController.swift
@@ -0,0 +1,13 @@
+//
+//  AmiiboController.swift
+//  Amiibo
+//
+//  Created by morse on 5/29/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+class AmiiboController {
+    
+}
---
+++ Amiibo/Model/Amiibo.swift
@@ -0,0 +1,16 @@
+//
+//  Amiibo.swift
+//  Amiibo
+//
+//  Created by morse on 5/29/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+struct Amiibo {
+    let name: String
+    let gameSeries: String
+    let type: String
+    let image: UIImage
+}
